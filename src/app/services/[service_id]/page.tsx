2bf7316d1b698c29c4aa4e271f3d3eba7d39ee64
jugol karmakar <jugol.cse@gmail.com>
2024-03-12 01:26:03 +0600

    single page commit

--- src/app/services/[service_id]/page.tsx
+++ src/app/services/[service_id]/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ServiceHero from "../_components/hero";
+import ServicePrice from "../_components/service-price";
+import ServicesCard from "../_components/services-card";
 
 export default function SingleService() {
-  return <div>service id component</div>;
+  return (
+    <>
+      <ServiceHero />
+      <ServicePrice />
+      <ServicesCard />
+    </>
+  );
 }
