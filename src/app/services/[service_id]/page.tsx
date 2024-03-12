a68c6cd7d166f8be95d86993322e07f777e13d43
jugol karmakar <jugol.cse@gmail.com>
2024-03-12 14:11:31 +0600

    service page commit

--- src/app/services/[service_id]/page.tsx
+++ src/app/services/[service_id]/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ServiceHero from "../_components/hero";
 import ServicePrice from "../_components/service-price";
 import ServicesCard from "../_components/services-card";
+import StayCompliant from "../_components/stay-compliant";
+import TrustedPartner from "../_components/trusted-partner";
+import ServiceAbout from "../_components/about";
 
 export default function SingleService() {
   return (
@@ -9,6 +12,9 @@ export default function SingleService() {
       <ServiceHero />
       <ServicePrice />
       <ServicesCard />
+      <ServiceAbout />
+      <TrustedPartner />
+      <StayCompliant />
     </>
   );
 }
