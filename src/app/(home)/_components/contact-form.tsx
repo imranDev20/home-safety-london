ca6c0a841008f112e77731d224205156618dccda
jugol karmakar <jugol.cse@gmail.com>
2024-03-12 23:32:16 +0600

    contact page

--- src/app/(home)/_components/contact-form.tsx
+++ src/app/(home)/_components/contact-form.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/joy";
+import { Box, Container, FormControl, Grid, Input, Typography } from "@mui/joy";
 import { useForm } from "react-hook-form";
 
 export default function ContactForm() {
   const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <Container>
@@ -14,7 +15,30 @@ export default function ContactForm() {
           best safety certificate for your needs.
         </Typography>
       </Box>
-      <Box></Box>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2}>
+          <Grid xs={5}>
+            <FormControl>
+              <Input name="firstname" type="text" placeholder="First Name" />
+            </FormControl>
+          </Grid>
+          <Grid xs={5}>
+            <FormControl>
+              <Input name="lastname" type="text" placeholder="Last Name" />
+            </FormControl>
+          </Grid>
+          <Grid xs={5}>
+            <FormControl>
+              <Input name="email" type="email" placeholder="Email Address" />
+            </FormControl>
+          </Grid>
+          <Grid xs={5}>
+            <FormControl>
+              <Input name="phone" type="number" placeholder="Phone Number" />
+            </FormControl>
+          </Grid>
+        </Grid>
+      </form>
     </Container>
   );
 }
