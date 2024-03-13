5f9c55f8021306c1dae448d3f2bf727fcc8061e2
jugol karmakar <jugol.cse@gmail.com>
2024-03-13 13:55:29 +0600

    testimonial commit

--- src/app/(home)/_components/contact-form.tsx
+++ src/app/(home)/_components/contact-form.tsx
@@ -1,44 +1,130 @@
 import React from "react";
 import { Box, Container, FormControl, Grid, Input, Typography } from "@mui/joy";
 import { useForm } from "react-hook-form";
+import Image from "next/image";
+import londonView from "../../../images/london-view.jpg";
 
 export default function ContactForm() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data: any) => console.log(data);
 
   return (
-    <Container>
-      <Box>
-        <Typography>Get In Touch</Typography>
-        <Typography>
-          Our friendly customer service team are ready to help you choose the
-          best safety certificate for your needs.
-        </Typography>
+    <Box
+      sx={{
+        position: "relative",
+        my: 5,
+      }}
+    >
+      <Image
+        alt="londonView"
+        src={londonView}
+        sizes="100vw"
+        fill
+        loading="lazy"
+        placeholder="blur"
+        style={{ objectFit: "cover" }}
+      />
+      <Box
+        sx={{
+          height: "100%",
+          position: "relative",
+          pt: 10,
+          pb: 10,
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            top: 0,
+            left: 0,
+            backgroundColor: "black",
+            opacity: 0.6,
+          },
+        }}
+      >
+        <Container
+          sx={{
+            backgroundColor: "white",
+            opacity: 0.9,
+            borderRadius: 15,
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <Box sx={{ textAlign: "center", py: 3 }}>
+            <Typography>Get In Touch</Typography>
+            <Typography>
+              Our friendly customer service team are ready to help you choose
+              the best safety certificate for your needs.
+            </Typography>
+          </Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={3}>
+              <Grid xs={7}>
+                <Box>
+                  <Grid container spacing={2}>
+                    <Grid xs={6}>
+                      <FormControl>
+                        <Input
+                          name="firstname"
+                          type="text"
+                          placeholder="First Name"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={6}>
+                      <FormControl>
+                        <Input
+                          name="lastname"
+                          type="text"
+                          placeholder="Last Name"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={6}>
+                      <FormControl>
+                        <Input
+                          name="email"
+                          type="email"
+                          placeholder="Email Address"
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={6}>
+                      <FormControl>
+                        <Input
+                          name="phone"
+                          type="number"
+                          placeholder="Phone Number"
+                        />
+                      </FormControl>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Grid>
+              <Grid xs={5}>
+                <Box>
+                  <Typography>Visit us</Typography>
+                  <Typography>Information Technology Building</Typography>
+                  <Typography>+123-456-7890</Typography>
+                  <Typography>info@homesafetylondon.co.uk</Typography>
+                </Box>
+                <Box sx={{ mt: 2 }}>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
+                    style={{
+                      width: "100%",
+                      height: "300px",
+                      border: 0,
+                      borderRadius: 15,
+                      overflow: "hidden",
+                    }}
+                  ></iframe>
+                </Box>
+              </Grid>
+            </Grid>
+          </form>
+        </Container>
       </Box>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
-          <Grid xs={5}>
-            <FormControl>
-              <Input name="firstname" type="text" placeholder="First Name" />
-            </FormControl>
-          </Grid>
-          <Grid xs={5}>
-            <FormControl>
-              <Input name="lastname" type="text" placeholder="Last Name" />
-            </FormControl>
-          </Grid>
-          <Grid xs={5}>
-            <FormControl>
-              <Input name="email" type="email" placeholder="Email Address" />
-            </FormControl>
-          </Grid>
-          <Grid xs={5}>
-            <FormControl>
-              <Input name="phone" type="number" placeholder="Phone Number" />
-            </FormControl>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+    </Box>
   );
 }
--- src/app/(home)/_components/testimonial.tsx
+++ src/app/(home)/_components/testimonial.tsx
@@ -3,85 +3,82 @@ import { Box, Card, CardContent, Container, Typography } from "@mui/joy";
 import StarIcon from "@mui/icons-material/Star";
 import { CarouselProvider, Slider, Slide, DotGroup } from "pure-react-carousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
-
-const REVIEW_ITEM = [
-  {
-    id: 1,
-    name: "Shaun Smith",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 2,
-    name: "Emily Johnson",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 3,
-    name: "Noah Williams",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 4,
-    name: "Olivia Brown",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 5,
-    name: "Ethan Jones",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 6,
-    name: "Benjamin Miller",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 7,
-    name: "Sophia Wilson",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-  {
-    id: 8,
-    name: "Babel Davis",
-    intro: "Had an EICR Check",
-    review:
-      "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
-  },
-];
+import FormatQuoteIcon from "@mui/icons-material/FormatQuote";
 
 export default function Testimonial() {
+  const REVIEW_ITEM = [
+    {
+      id: 1,
+      name: "Shaun Smith",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 2,
+      name: "Emily Johnson",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 3,
+      name: "Noah Williams",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 4,
+      name: "Olivia Brown",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 5,
+      name: "Ethan Jones",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 6,
+      name: "Benjamin Miller",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 7,
+      name: "Sophia Wilson",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+    {
+      id: 8,
+      name: "Babel Davis",
+      intro: "Had an EICR Check",
+      review:
+        "I was very impressed in regards to the service provided. They are very professional, technician visit was punctual and the EICR report was complete and quickly released. I will use them again next time for sure.",
+    },
+  ];
   return (
-    <Container component="section" sx={{ paddingY: "20px" }}>
+    <Container sx={{ py: 5 }}>
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
-          marginTop: "20px",
-          marginBottom: "10px",
+          py: 2,
         }}
       >
-        <Typography
-          sx={{ fontSize: "24px", fontWeight: 600, paddingBottom: "10px" }}
-        >
+        <Typography sx={{ fontSize: 30, fontWeight: 600 }}>
           What our customers say about us
         </Typography>
-        <Typography>
+        <Typography sx={{}}>
           {[...Array(5)].map((_, index) => (
             <StarIcon key={index} sx={{ color: "yellow" }} />
           ))}
@@ -89,74 +86,90 @@ export default function Testimonial() {
       </Box>
 
       <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={50}
-        totalSlides={8}
+        naturalSlideWidth={400}
+        naturalSlideHeight={200}
+        isIntrinsicHeight={true}
+        totalSlides={REVIEW_ITEM.length}
+        visibleSlides={3}
         infinite
         isPlaying
         interval={5000}
       >
         <Slider>
-          {REVIEW_ITEM.map((review, index) => (
+          {REVIEW_ITEM.map((slides, index) => (
             <Slide index={0} key={index}>
-              <Container maxWidth="md">
-                <Box>
-                  <Card variant="outlined">
-                    <CardContent>
-                      <Typography sx={{ fontSize: "16px", paddingY: "8px" }}>
-                        {review.review}
-                      </Typography>
-                      <Box sx={{ marginTop: "10px" }}>
-                        <Typography sx={{ fontSize: "18px" }}>
-                          {review.name}
-                        </Typography>
-                        <Typography>
-                          {[...Array(5)].map((_, index) => (
-                            <StarIcon key={index} sx={{ color: "yellow" }} />
-                          ))}
-                        </Typography>
-                        <Typography sx={{ fontWeight: 600, fontSize: "14px" }}>
-                          {review.intro}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Box>
-              </Container>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+
+                  mx: 2,
+                }}
+              >
+                <Card variant="soft" sx={{ textAlign: "center", p: 4 }}>
+                  <Box>
+                    <Typography sx={{ fontSize: 20, fontWeight: 600 }}>
+                      {slides.name}
+                    </Typography>
+                    <Typography sx={{ fontSize: 20, fontWeight: 600 }}>
+                      {slides.intro}
+                    </Typography>
+                    <Typography sx={{ my: "3px" }}>
+                      {[...Array(5)].map((_, index) => (
+                        <StarIcon key={index} sx={{ color: "yellow" }} />
+                      ))}
+                    </Typography>
+
+                    <FormatQuoteIcon sx={{ fontSize: 50, color: "#0b6bcb" }} />
+                    <Typography sx={{ textAlign: "center", fontSize: 18 }}>
+                      {slides.review}
+                    </Typography>
+                  </Box>
+                </Card>
+              </Box>
             </Slide>
           ))}
         </Slider>
         <Box
           sx={{
             display: "flex",
-            alignItems: "center",
             justifyContent: "center",
-
-            "& .carousel__dot": {
-              backgroundColor: "#c3c4ca",
-              border: "1px",
-              width: "9px",
-              height: "9px",
-              margin: "0px 4px",
-              padding: "0px",
-              borderRadius: "50%",
-            },
-            "& .carousel__dot--selected": {
-              width: "20px",
-              height: "8px",
-              borderRadius: "10px",
-              backgroundColor: "#6267a1",
-            },
+            alignItems: "center",
+            mt: 2,
           }}
         >
-          <DotGroup
-            style={{
+          <Box
+            sx={{
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
-              marginBottom: "20px",
+
+              "& .carousel__dot": {
+                backgroundColor: "#c3c4ca",
+                border: "1px",
+                width: "9px",
+                height: "9px",
+                margin: "0px 4px",
+                padding: "0px",
+                borderRadius: "50%",
+              },
+              "& .carousel__dot--selected": {
+                width: "20px",
+                height: "8px",
+                borderRadius: "10px",
+                backgroundColor: "#6267a1",
+              },
             }}
-          />
+          >
+            <DotGroup
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            />
+          </Box>
         </Box>
       </CarouselProvider>
     </Container>
