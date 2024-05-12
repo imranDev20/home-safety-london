af7af452773033ffb564cff433565210667028f3
imranDev20 <imrandev20@gmail.com>
2024-05-13 04:52:28 +0600

    Add testimonial model

--- src/app/api/_models/User.ts
+++ src/app/api/_models/User.ts
@@ -1,54 +1,63 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-interface User {
+interface IUser {
   name: string;
   email: string;
-  role: "user" | "admin";
+  role: "customer" | "admin" | "staff";
   isPasswordCreated: boolean;
-  password?: string;
-  addresses?: mongoose.Types.ObjectId[];
+  password: string;
+  addresses: mongoose.Types.ObjectId[];
   orders: mongoose.Types.ObjectId[];
+  preferences: {
+    mode: "light" | "dark";
+  };
 }
 
-const userSchema = new mongoose.Schema<User>({
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  role: {
-    type: String,
-    required: true,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-  isPasswordCreated: {
-    type: Boolean,
-    default: false,
-  },
-  password: {
-    type: String,
-    required: function (this: User) {
-      return this.isPasswordCreated;
+const userSchema: Schema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
     },
-  },
-  orders: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order",
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
+    role: {
+      type: String,
+      required: true,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
 
-  addresses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Address",
+    addresses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+      },
+    ],
+    preferences: {
+      mode: {
+        type: String,
+        enum: ["light", "dark"],
+        default: "light",
+      },
     },
-  ],
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
